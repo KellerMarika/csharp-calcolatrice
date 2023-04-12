3f4d3c58f36b32f28585de374f41d0d910fd5d3f
KellerMarika <marikakeller92@gmail.com>
2023-04-12 17:02:28 +0200

    Add project files.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+
+        //Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto oggi…Quale?
+        //Overload
+
+
+        //Somma di due numeri interi
+        public static int sum(int n1, int n2) => n1 + n2;
+        //Somma di due numeri double
+        public static double sum(double n1, double n2) => n1 + n2;
+
+
+        //Differenza di due numeri interi
+        public static int diff(int n1, int n2) => n1 - n2;
+        //Differenza di due numeri double
+        public static double diff(double n1, double n2) => n1 - n2;
+
+
+        //Moltiplicazione di due numeri interi
+        public static int mult(int n1, int n2) => n1 * n2;
+
+        //Moltiplicazione di due numeri double
+        public static double mult(double n1, double n2) => n1 * n2;
+
+
+        //Valore assoluto di un numero intero
+       // public static int abs(this int n)=>   Math.Abs(n);
+        //Valore assoluto di un numero double
+       // public static double abs(this double n) => Math.Abs(n);
+
+
+
+        public static int abs(this int n)
+        {
+
+           string nString = Convert.ToString(n);
+            //  Console.WriteLine(nString);
+
+            if (nString.Contains("-"))
+            {
+                return Convert.ToInt16(nString.Remove(0,1));
+            }
+            else
+            {
+                return n;
+            }           
+        }
+
+        public static double abs(this double n)
+        {
+
+            string nString = Convert.ToString(n);
+            //  Console.WriteLine(nString);
+
+            if (nString.Contains("-"))
+            {
+                return Convert.ToDouble(nString.Remove(0, 1));
+                
+            }
+            else
+            {
+                return n;
+            }
+        }
+
+        //Minimo tra due numeri interi
+        public static int min(int n1, int n2) =>n1 <= n2? n1:n2;
+        //Minimo tra due numeri double
+        public static double min(double n1, double n2) => n1 <= n2 ? n1 : n2;
+
+
+        //Massimo tra due numeri interi
+        public static int max(int n1, int n2) => n1 >= n2 ? n1 : n2;
+        //Massimo tra due numeri double
+        public static double max(double n1, double n2) => n1 >= n2 ? n1 : n2;
+
+        //BONUS:
+
+        public static double pow(this int n, int esp)
+        {
+            n= (n == 0 )?  1 : n;
+            if (esp == 0) return 1;
+            double nPow = 0;
+
+            if (esp > 0) 
+                for (int i = 1; i < esp; i++)
+                    nPow += n * n;    
+            else
+            {
+                nPow = (1.0 / n);
+                for (int i =1; i < esp.abs(); i++)
+                 nPow *= (1.0/n);
+            }
+            return nPow;
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int i1= -1; int i2=2; int i3=3; int i4 = -1;
+            double d1 = - 3.5; double d2=2.3;
+
+            //Testate tutti i metodi della vostra classe di helper(con un esempio per ogni casistica).
+
+            Console.WriteLine($"somma int: {CalcoliHelper.sum(i1, i2)}");
+            Console.WriteLine($"somma double: {CalcoliHelper.sum(d1, d2)}");
+
+            Console.WriteLine($"differenza int: {CalcoliHelper.diff(i1, i2)}");
+            Console.WriteLine($"differenza double: {CalcoliHelper.diff(d1, d2)}");
+
+            Console.WriteLine($"moltiplicazione int: {CalcoliHelper.mult(i1, i2)}");
+            Console.WriteLine($"moltiplicazione double: {CalcoliHelper.mult(d1, d2)}");
+
+            Console.WriteLine($"v assoluto int: {i1.abs()}");
+            Console.WriteLine($"v assoluto double: {d1.abs()}");
+
+            Console.WriteLine($"minimo int: {CalcoliHelper.min(i1, i2)}");
+            Console.WriteLine($"minimo double: {CalcoliHelper.min(d1, d2)}");
+
+            Console.WriteLine($"max int: {CalcoliHelper.max(i1, i2)}");
+            Console.WriteLine($"max double: {CalcoliHelper.max(d1, d2)}");
+
+            Console.WriteLine($"potenza di {i2}^ {i3} = {i2.pow(i3)}");
+            Console.WriteLine($"potenza di {i2}^ {i4} = {i2.pow(i4)}");
+           
+
+            string aaa = Convert.ToString(i1);
+            Console.WriteLine("stringa: "+aaa);
+            Console.WriteLine(aaa.Contains("-"));
+            Console.WriteLine(aaa.IndexOf("-"));
+
+           string x=  aaa.Remove(0,1);
+           // Console.WriteLine(aaa.Remove(0));
+            Console.WriteLine("post remove "+aaa);
+            Console.WriteLine("rest " + x);
+        }
+    }
+}
